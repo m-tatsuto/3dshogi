d90eb9a6b719ebe7e9abbf57ea851b0ca55088df
m-tatsuto <m.tatsuto@gmail.com>
2013-05-01 19:18:05 +0900

    ファイル整理

---
+++ js/gameStrat.js
@@ -0,0 +1,8 @@
+function gameStartTest(){
+	turncamp = 0;
+	touchComaObject = null;
+	selectStockFlag = false;
+	setInitMapObject();
+	testMapDisplay();
+	setThreeJs3dMapObject();
+}
---
+++ js/selectComa.js
@@ -0,0 +1,20 @@
+var nowSelectPosition = new Array();
+
+function selectPosition( spx, spy, spz ){
+  var positionX = spx;
+  var positionY = spy;
+  var positionZ = spz;
+
+  var successFlag = selectComaObject(positionX,positionY,positionZ);
+
+  if ( !successFlag ) {
+    touchComaObject = null;
+    selectStockFlag = false;
+  }
+
+  nowSelectPosition[0] = positionX;
+  nowSelectPosition[1] = positionY;
+  nowSelectPosition[2] = positionZ;
+
+  testMapDisplay();
+}
